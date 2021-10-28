b754289f6227f818b68565a5a3d866d111cc7686
Gaya Blau <gayablau@gmail.com>
2021-10-28 18:17:15 +0300

    merge notify utils with notify service

--- app/src/main/java/com/example/androidgaya/util/NotificationUtils.kt
+++ app/src/main/java/com/example/androidgaya/util/NotificationUtils.kt
@@ -1,16 +1,94 @@
 package com.example.androidgaya.util
 
-import android.app.Activity
-import android.app.AlarmManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.example.androidgaya.R
 import com.example.androidgaya.main.notify.ReminderReceiver
+import com.example.androidgaya.main.ui.MainActivity
 import com.example.androidgaya.repositories.models.ReminderEntity
 import java.util.*
 
+
 class NotificationUtils {
+
+    companion object {
+        const val CHANNEL_ID = "samples.notification.devdeeds.com.CHANNEL_ID"
+        const val CHANNEL_NAME = "Sample Notification"
+    }
+
+    private lateinit var mNotification: Notification
+    var id: Int = 0
+
+    private fun createChannel(context: Context) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance)
+            notificationChannel.enableVibration(true)
+            notificationChannel.setShowBadge(true)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.parseColor("#e8334a")
+            notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    fun notify(context: Context, intent: Intent) {
+        createChannel(context)
+        var timestamp: Long = 0
+        var header = ""
+        var description = ""
+
+        if (intent != null) {
+            if (intent.extras != null) {
+                timestamp = intent.extras!!.getLong(context.getString(R.string.timestamp))
+                header = intent.extras!!.getString(context.getString(R.string.header_uppercase)).toString()
+                description = intent.extras!!.getString(context.getString(R.string.description_uppercase)).toString()
+                id = intent.extras!!.getInt(context.getString(R.string.id_uppercase))
+            }
+        }
+        if (timestamp > 0) {
+            val notifyIntent = Intent(context, MainActivity::class.java)
+
+            notifyIntent.putExtra(context.getString(R.string.header_uppercase), header)
+            notifyIntent.putExtra(context.getString(R.string.description_uppercase), description)
+            notifyIntent.putExtra(context.getString(R.string.id_uppercase), id)
+            notifyIntent.putExtra(context.getString(R.string.notification), true)
+
+            notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = timestamp
+
+            val pendingIntent = PendingIntent.getActivity(context,
+                    id,
+                    notifyIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT)
+            val res = context.resources
+
+            mNotification = NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.clock_icon)
+                    .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.clock_notify2))
+                    .setAutoCancel(true)
+                    .setContentTitle(header)
+                    .setContentText(description).build()
+
+            val notificationManager: NotificationManager =
+                    context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(id, mNotification)
+
+
+    }}
+
     fun setExistNotification(timeInMilliSeconds: Long,
                              context: Context,
                              header: String,
